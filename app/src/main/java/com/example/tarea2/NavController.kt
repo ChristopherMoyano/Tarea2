b49cc118b3f20b531ad9d502acca16659b5cebce
Christopher Moyano <cmoyano@ing.ucsc.cl>
2025-04-20 23:46:54 -0400

    terminado por ahora

--- app/src/main/java/com/example/tarea2/NavController.kt
+++ app/src/main/java/com/example/tarea2/NavController.kt
@@ -44,8 +44,40 @@ fun Navigation(){
                 title = "Accion",
                 icon = R.drawable.accion,
                 contenido = mutableListOf(
-                    Contenido(0,"Naruto",R.drawable.naruto,"un ninja legendario"),
-                    Contenido(1,"Bleach",R.drawable.bleach,"Shinigamis y espadas")
+                    Contenido(0,"Naruto",R.drawable.naruto,
+                        "Naruto, un aprendiz de ninja de la Aldea Oculta de " +
+                                "Konoha es un chico travieso que desea llegar a ser el " +
+                                "Hokage de la aldea para demostrar a todos lo que vale. " +
+                                "Lo que descubre al inicio de la historia es que la gente " +
+                                "le mira con desconfianza porque en su interior está " +
+                                "encerrado el demonio Kyubi que una vez destruyó la aldea, " +
+                                "y que el anterior líder de la misma tuvo que encerrar en su " +
+                                "cuerpo siendo aún muy pequeño, a coste de su vida. Aunque sus " +
+                                "compañeros no saben esto, tampoco le aprecian porque es mal " +
+                                "estudiante y siempre está haciendo bromas. " +
+                                "Sin embargo, la forma de actuar y la determinación de Naruto " +
+                                "demuestran a los demás que puede llegar muy lejos, y el " +
+                                "recelo de los otros chicos se va disipando. Naruto y sus " +
+                                "compañeros Sakura y Sasuke, junto a su maestro Kakashi " +
+                                "tendrán que enfrentarse a una serie de combates y misiones a " +
+                                "lo largo de la historia que les permitirán mejorar y crecer. " +
+                                "Naruto se vera enfrentado a sus principales enemigos " +
+                                "Akatsuki, Itachi y Kisame."),
+                    Contenido(1,"Bleach",R.drawable.bleach,
+                        "Kurosaki Ichigo es un estudiante de instituto de 15 años, " +
+                                "que tiene una peculiaridad: es capaz de ver, oír y hablar " +
+                                "con fantasmas. Pero no sabe hasta dónde puede abarcar la " +
+                                "clasificación de espíritus, ni lo que conlleva el saberlo. " +
+                                "Un buen día, una extraña chica de pequeña estatura que " +
+                                "viste ropas negras de samurai entra en su cuarto. " +
+                                "Se llama Rukia Kuchiki, y es una Shinigami (Dios de la Muerte). " +
+                                "Ante la incredulidad de Ichigo, le explica que su trabajo es " +
+                                "mandar a las almas buenas o plus a un lugar llamado la Sociedad " +
+                                "de Almas, y eliminar a las almas malignas o hollows. " +
+                                "Luego junto a Inoue Orihime, Ishida Ury y Sado Yasutora se " +
+                                "veran envueltos en diferentes batallas, las cuales iran " +
+                                "desarrollando sus diferentes habilidades que le otorgaran a " +
+                                "cada uno su importancia en la serie.")
                 )
             )
         )
@@ -54,7 +86,26 @@ fun Navigation(){
                 id =1,
                 title = "Romance",
                 icon = R.drawable.romance,
-                contenido = mutableListOf()
+                contenido = mutableListOf(
+                    Contenido(0,"Danjo no Yuujou wa Seiritsu suru?",R.drawable.danjo_no_yuujou_wa_seiritsu_suru,
+                        description ="La amistad de la infancia entre Himari y " +
+                                "Yu se tambalea cuando Yu se reencuentra en preparatoria " +
+                                "con su primer amor. Himari, que nunca experimentó la emoción " +
+                                "del amor, ahora debe enfrentar sus propios sentimientos. " +
+                                "Sus sueños compartidos y los tranquilos días en su club de " +
+                                "jardinería se ponen a prueba en esta historia con amor, " +
+                                "flores y los altibajos que supone madurar." ),
+                    Contenido(1, title = "Class no Daikirai na Joshi to Kekkon suru Koto ni Natta",
+                        R.drawable.class_no_daikirai_na_joshi_to_kekkon_suru,
+                        description = "El estudiante de preparatoria Hojo Saito " +
+                                "está a punto de heredar la gran empresa de su abuelo. Para ello " +
+                                "debe casarse con Akane Sakuramori, la chica a la que más detesta y " +
+                                "que a su vez le detesta a él. Los dos están decididos a " +
+                                "ocultar su imprevisto matrimonio a sus compañeros, " +
+                                "pero al comenzar su nueva vida como pareja, la distancia que los " +
+                                "separa comienza a reducirse." )
+
+                )
             )
         )
         add(
@@ -62,7 +113,26 @@ fun Navigation(){
                 id =2,
                 title = "Fantasia",
                 icon = R.drawable.fantasia,
-                contenido = mutableListOf()
+                contenido = mutableListOf(
+                    Contenido(0,"Girumasu",R.drawable.girumasu,
+                        description ="Alina pensaba que había encontrado el trabajo perfecto como " +
+                                "recepcionista del gremio. Un trabajo estable, tranquilo y con un " +
+                                "uniforme monísimo. Pero este trabajo de ensueño se convierte en " +
+                                "una pesadilla cada vez que los aventureros se quedan atrapados " +
+                                "en el proceso de completar una mazmorra. Cansada de trabajar " +
+                                "hasta las tantas por las noches, Alina decide acabar con los jefes " +
+                                "ella misma. Sus impresionantes habilidades le han hecho ganarse el " +
+                                "apodo de Verdugo. ¿Podrá mantener su identidad en secreto?" ),
+                    Contenido(1,"Farmagia",R.drawable.farmagia,
+                        "En Felicidad, unos granjeros conocidos como los Farmagia crían" +
+                                " monstruos bajo el pacífico gobierno del Magus Diluculum. " +
+                                "Tras la muerte del Magus, estalla una lucha de poder entre " +
+                                "fuerzas que utilizan monstruos para hacerse con el control. " +
+                                "En la ciudad de Centvelt, el Farmagia Ten y sus amigos se " +
+                                "unen para enfrentarse al nuevo y déspota gobernante, Glaza. " +
+                                "Ten, sus amigos y los monstruos que han criado deberán " +
+                                "mantenerse firmes para defender su libertad.")
+                )
             )
         )
     }}
